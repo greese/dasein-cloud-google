25979e76c34a5c8c3ddb06a94c22ff6a090f6072
Roger Unwin <roger.unwin@enstratius.com>
2014-06-30 10:35:08 -0700

    allow for multiple forwarding rules per load balancer. NOTE address reflects only the last forwarding rule seen... dasein cant yet cope with multiple addresses.

--- src/main/java/org/dasein/cloud/google/network/LoadBalancerSupport.java
+++ src/main/java/org/dasein/cloud/google/network/LoadBalancerSupport.java
@@ -124,9 +124,10 @@ public class LoadBalancerSupport extends AbstractLoadBalancerSupport<Google>  {
     	APITrace.begin(provider, "LB.removeLoadBalancer");
 		gce = provider.getGoogleCompute();
 
-		String forwardingRuleName = getForwardingRule(loadBalancerId);
-		if (forwardingRuleName != null)
-			removeLoadBalancerForwardingRule(forwardingRuleName); 
+		List<String> forwardingRuleNames = getForwardingRule(loadBalancerId);
+		for (String forwardingRuleName : forwardingRuleNames)
+			if (forwardingRuleName != null)
+				removeLoadBalancerForwardingRule(forwardingRuleName); 
     	String healthCheckName = getLoadBalancerHealthCheckName(loadBalancerId);
 
         try {
@@ -172,10 +173,11 @@ public class LoadBalancerSupport extends AbstractLoadBalancerSupport<Google>  {
 		return healthCheck;
     }
 
-    private String getForwardingRule(String targetPoolName) throws CloudException, InternalException {
+    private List<String> getForwardingRule(String targetPoolName) throws CloudException, InternalException {
     	APITrace.begin(provider, "LB.getForwardingRule");
         gce = provider.getGoogleCompute();
 
+        List<String> forwardingRuleNames = new ArrayList<String>();
         try {
 			ForwardingRuleList result = gce.forwardingRules().list(ctx.getAccountNumber(), ctx.getRegionId()).execute();
 			for (ForwardingRule fr : result.getItems()) {
@@ -183,7 +185,7 @@ public class LoadBalancerSupport extends AbstractLoadBalancerSupport<Google>  {
 				forwardingRuleTarget = forwardingRuleTarget.substring(forwardingRuleTarget.lastIndexOf("/") + 1);
 
 				if (targetPoolName.equals(forwardingRuleTarget)) 
-					return fr.getName();
+					forwardingRuleNames.add(fr.getName());
 			}
 		} catch (IOException e) {
 			if (e.getClass() == GoogleJsonResponseException.class) {
@@ -194,7 +196,7 @@ public class LoadBalancerSupport extends AbstractLoadBalancerSupport<Google>  {
 		} finally {
             APITrace.end();
         }
-        return null;
+        return forwardingRuleNames;
     }
 
     private void removeLoadBalancerForwardingRule(String forwardingRuleName) throws CloudException, InternalException {
@@ -782,18 +784,20 @@ public class LoadBalancerSupport extends AbstractLoadBalancerSupport<Google>  {
 		int ports[] = null;
 		List<LbListener> listeners = new ArrayList<LbListener>();
 		try {
-			fr = gce.forwardingRules().get(ctx.getAccountNumber(), ctx.getRegionId(), tp.getName()).execute();
-			forwardingRuleAddress = fr.getIPAddress();
-			//foreardingRuleProtocol = fr.getIPProtocol();
-			forwardingRulePortRange = fr.getPortRange();
-
-			ports = portsToRange(forwardingRulePortRange);
-			String protocol = fr.getIPProtocol();
-			if (protocol.equals("TCP"))
-				protocol = "RAW_TCP";
-			for (int port : ports) 
-				// Hard Coded Algorithm and persistence, havent found a dynamic source yet.
-				listeners.add(LbListener.getInstance(LbAlgorithm.SOURCE, LbPersistence.SUBNET, LbProtocol.valueOf(protocol), port, port));
+			List<String> forwardingRuleNames = getForwardingRule(tp.getName());
+			for (String forwardingRuleName : forwardingRuleNames) {
+				fr = gce.forwardingRules().get(ctx.getAccountNumber(), ctx.getRegionId(), forwardingRuleName).execute();
+				forwardingRuleAddress = fr.getIPAddress();
+				//foreardingRuleProtocol = fr.getIPProtocol();
+				forwardingRulePortRange = fr.getPortRange();
+				ports = portsToRange(forwardingRulePortRange);
+				String protocol = fr.getIPProtocol();
+				if (protocol.equals("TCP"))
+					protocol = "RAW_TCP";
+				for (int port : ports) 
+					// Hard Coded Algorithm and persistence, havent found a dynamic source yet.
+					listeners.add(LbListener.getInstance(LbAlgorithm.SOURCE, LbPersistence.SUBNET, LbProtocol.valueOf(protocol), port, port));
+			}
 		} catch (IOException e) {
 			// Guess no forwarding rules for this one.
 		}
