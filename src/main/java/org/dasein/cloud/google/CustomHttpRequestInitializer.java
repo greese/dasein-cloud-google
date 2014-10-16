0079ce1a0bfcd8072741cbd82e74d53c8c435a65
Roger Unwin <roger.unwin@enstratius.com>
2014-10-15 22:22:42 -0700

    best try so far on getRequestTrackingStrategy + InheritableThreadLocal + caching optimizations.

--- src/main/java/org/dasein/cloud/google/CustomHttpRequestInitializer.java
+++ src/main/java/org/dasein/cloud/google/CustomHttpRequestInitializer.java
@@ -7,44 +7,40 @@ import java.io.IOException;
 
 import org.dasein.cloud.ProviderContext;
 import org.dasein.cloud.RequestTrackingStrategy;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
-import java.lang.ThreadLocal;
+
+import java.lang.InheritableThreadLocal;
 /**
  * @author unwin
  *
  */
 public class CustomHttpRequestInitializer implements HttpRequestInitializer {
-    /* (non-Javadoc)
-     * @see com.google.api.client.http.HttpRequestInitializer#initialize(com.google.api.client.http.HttpRequest)
-     */
-    private HttpRequestInitializer stackedRequestInitializer = null;
     private InheritableThreadLocal<ProviderContext> context = null;
+    private InheritableThreadLocal<GoogleCredential> credentials = null;
 
     CustomHttpRequestInitializer() {
         context = new InheritableThreadLocal<ProviderContext>();
+        credentials = new InheritableThreadLocal<GoogleCredential>();
     }
 
     @Override
     public void initialize( HttpRequest request ) throws IOException {
-        stackedRequestInitializer.initialize(request);
         HttpHeaders headers = request.getHeaders();
+        credentials.get().initialize(request);
 
-        ProviderContext ctx = context.get();
-
-        RequestTrackingStrategy strategy = ctx.getRequestTrackingStrategy();
-        if(strategy != null && strategy.getSendAsHeader()){
+        RequestTrackingStrategy strategy = context.get().getRequestTrackingStrategy();
+        if (strategy != null && strategy.getSendAsHeader()) {
             headers.put(strategy.getHeaderName(), strategy.getRequestId());
             request.setHeaders(headers);
         }
     }
 
-    public void setStachedRequestInitializer(HttpRequestInitializer requestInitializer) {
-        stackedRequestInitializer = requestInitializer;
-    }
-
-    public void setContext(ProviderContext ctx) {
+    public void setStackedRequestInitializer(ProviderContext ctx, GoogleCredential cred) {
         context.set(ctx);
+        credentials.set(cred); 
     }
 }
