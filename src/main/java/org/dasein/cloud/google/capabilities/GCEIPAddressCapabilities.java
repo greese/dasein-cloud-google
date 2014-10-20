2650ce68ef77a3146b21bf2f3460d474920541c4
Roger Unwin <roger.unwin@enstratius.com>
2014-10-20 11:31:15 -0700

    disabled isForwarding FB 5346

--- src/main/java/org/dasein/cloud/google/capabilities/GCEIPAddressCapabilities.java
+++ src/main/java/org/dasein/cloud/google/capabilities/GCEIPAddressCapabilities.java
@@ -59,7 +59,7 @@ public class GCEIPAddressCapabilities extends AbstractCapabilities<Google> imple
     }
 
     @Override public boolean isForwarding(IPVersion version) throws CloudException, InternalException{
-        if(version.equals(IPVersion.IPV4))return true;
+        // cannot port forward from one external ip to a specific vm without using load balancer magic.
         return false;
     }
 
